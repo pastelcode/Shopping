4ac0af024241bbb7edd5a611c3fb43138c346847
Pastel <pastel.dev.gt@gmail.com>
2024-07-28 21:52:36 -0600

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Samuel Marroquín on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
